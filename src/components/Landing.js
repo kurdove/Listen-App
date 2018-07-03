26748d1b28dca9f173b6c2925f9f00648b577443
Evgheni Curdov <kurdov.e@gmail.com>
2018-07-02 18:56:26 -0700

    Added content to landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,22 @@ import React from 'react';
 
 const Landing = () => (
     <section className="landing">
-    Landing page will go here
+      <h1 className="hero-title">Turn the musik up!</h1>
+
+      <section className="selling-points">
+        <div className="point">
+          <h2 className="point-title">Choose your music</h2>
+          <p className="point-description">The world is full of musik; why should you have to listen to music that someone else chose?</p>
+        </div>
+        <div className="point">
+          <h2 className="point-title">Unlimited, streaming, add-free</h2>
+          <p className="point-description">No arbitrary limits. No distraction.</p>
+        </div>
+        <div className="point">
+          <h2 className="point-title">Mobileenebled</h2>
+          <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+        </div>
+    </section>
     </section>
 );
 
